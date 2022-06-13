522ded1c0e0737c56c321c2ec386081c76526f5a
mici-que <Maria.Kohler@ibm.com>
2022-06-13 12:55:46 +0200

    green

--- test_testsonar.py
+++ test_testsonar.py
@@ -1,4 +1,9 @@
 from testsonar import main
+
 # initial test
 def test_init():
-    assert(main())==False
+    assert (main()) == False
+
+
+def test_input():
+    assert (main(1)) == True
--- testsonar.py
+++ testsonar.py
@@ -1,2 +1,4 @@
-def main():
+def main(num=None):
+    if num != None:
+        return True
     return False
